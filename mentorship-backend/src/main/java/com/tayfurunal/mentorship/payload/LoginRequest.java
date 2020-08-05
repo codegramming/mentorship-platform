30d7151b11dfef541fcc06e7081e79f424d99b99
tayfurunal <mtayfurunal@gmail.com>
2020-08-05 22:43:51 +0300

    create Login and Signup payload

---
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/payload/LoginRequest.java
@@ -0,0 +1,31 @@
+package com.tayfurunal.mentorship.payload;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    @Email
+    @NonNull
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @ApiModelProperty(required = true)
+    private String email;
+
+    @NonNull
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @ApiModelProperty(required = true)
+    private String password;
+}
---
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/payload/SignUpRequest.java
@@ -0,0 +1,37 @@
+package com.tayfurunal.mentorship.payload;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class SignUpRequest {
+    @NonNull
+    @NotBlank
+    @Size(min = 4, max = 16)
+    @ApiModelProperty(required = true)
+    private String username;
+
+    @Email
+    @NonNull
+    @NotBlank
+    @Size(min = 1, max = 129)
+    @ApiModelProperty(required = true)
+    private String email;
+
+    @NonNull
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @ApiModelProperty(required = true)
+    private String password;
+}
