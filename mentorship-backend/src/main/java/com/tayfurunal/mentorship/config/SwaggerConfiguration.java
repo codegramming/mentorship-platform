499a9a6b2f5c79a23a4635f6bfbd3b28207543ba
tayfurunal <mtayfurunal@gmail.com>
2020-08-05 12:38:02 +0300

    create Swagger Configuration

---
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/config/SwaggerConfiguration.java
@@ -0,0 +1,41 @@
+package com.tayfurunal.mentorship.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Mentorship App API Documentation")
+                .license("MIT")
+                .licenseUrl("https://opensource.org/licenses/MIT")
+                .version("1.0.0")
+                .contact(new Contact("Tayfur Unal", "https://tayfurunal.com", "mtayfurunal@gmail.com"))
+                .build();
+    }
+}
