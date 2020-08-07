444928b70055e912d3168b8a490798eff187ffe9
tayfurunal <mtayfurunal@gmail.com>
2020-08-07 17:24:35 +0300

    create admin panel

---
+++ mentorship-client/src/components/AdminPanel.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Header from './Layout/Header';
+import axios from 'axios';
+
+class AdminPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      applications: [],
+    };
+  }
+
+  componentDidMount() {
+    if (!this.props.security.roles[0].includes('ADMIN')) {
+      this.props.history.push('/');
+    } else {
+      this.getApplications();
+    }
+  }
+
+  getApplications = async () => {
+    let applications = await axios.get(`/api/mentors`);
+    this.setState({
+      applications: applications.data,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div className='projects'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <h1 className='display-4 text-center mt-4'>
+                  Pending Mentor Applications
+                </h1>
+                <hr />
+                <div class='job-list__wrapper mb-6'>
+                  {this.state.applications.map((application, index) => {
+                    return (
+                      <>
+                        <div key={index}>
+                          <a
+                            href='career-single.html'
+                            class='card p-0 mb-3 border-0 shadow-sm shadow--on-hover'
+                          >
+                            <div class='card-body'>
+                              <span class='row justify-content-between align-items-center'>
+                                <span class='col-md-5 color--heading'>
+                                  <span class='badge badge-circle background--success text-white mr-6'>
+                                    SE
+                                  </span>{' '}
+                                  <i class='fas fa-chalkboard-teacher mr-1'></i>
+                                  {application.user.displayName}
+                                </span>
+
+                                <span class='col-5 col-md-3 my-3 my-sm-0 color--text'>
+                                  <i class='fas fa-book-reader'></i>{' '}
+                                  {application.mainTopic}
+                                </span>
+
+                                <span class='col-7 col-md-3 my-3 my-sm-0 color--text'>
+                                  <i class='fad fa-ellipsis-h-alt'></i>{' '}
+                                  {console.log(application.mainTopic)}
+                                  {application.subTopics}
+                                </span>
+
+                                <span class='d-none d-md-block col-1 text-center color--text'>
+                                  <small>
+                                    <i class='fas fa-chevron-right'></i>
+                                  </small>
+                                </span>
+                              </span>
+                            </div>
+                          </a>
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+AdminPanel.propTypes = {
+  security: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  security: state.security,
+});
+
+export default connect(mapStateToProps)(AdminPanel);
