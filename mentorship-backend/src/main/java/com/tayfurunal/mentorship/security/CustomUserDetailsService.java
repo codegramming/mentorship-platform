6df39c24629588b5c4cffcc310b3a582a719e5f0
tayfurunal <mtayfurunal@gmail.com>
2020-08-08 12:32:02 +0300

    change auth request in security

--- mentorship-backend/src/main/java/com/tayfurunal/mentorship/security/CustomUserDetailsService.java
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/security/CustomUserDetailsService.java
@@ -23,9 +23,9 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
         if (user.getUsername().equals("admin")) {
             user.setAuthorities(Collections.singleton(Role.ADMIN));
             System.out.println("ADMIN CREATED");
