154d0722baf067fd6d6d76fda065f50c91e0228c
tayfurunal <mtayfurunal@gmail.com>
2020-08-08 12:46:14 +0300

    add init data

---
+++ mentorship-backend/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users VALUES (1000, 'ADMIN', 'admin@test.com', '$2a$10$F00U/hsXrqV/p/b1XDilDeGNhCAFbi6dXlMe503FGCjLuC0ttXMRW', 'admin');
+INSERT INTO users VALUES (1001, 'Tayfur Ünal', 'test@test.com', '$2a$10$F00U/hsXrqV/p/b1XDilDeGNhCAFbi6dXlMe503FGCjLuC0ttXMRW', 'user1');
