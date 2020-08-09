1ef1f2aceefa495872154fdf09e7465303afaffd
tayfurunal <mtayfurunal@gmail.com>
2020-08-09 14:30:20 +0300

    create add, get and remove apis for topic

--- mentorship-backend/src/main/java/com/tayfurunal/mentorship/controller/TopicController.java
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/controller/TopicController.java
@@ -6,6 +6,9 @@ import com.tayfurunal.mentorship.mapper.TopicMapper;
 import com.tayfurunal.mentorship.service.TopicService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +31,22 @@ public class TopicController {
 
     @PostMapping()
     ResponseEntity<?> createTopic(@Valid @RequestBody TopicDto topicDto) {
-        System.out.println(topicDto);
         Topic topic = TopicMapper.INSTANCE.toEntity(topicDto);
-        System.out.println(topic);
         return topicService.createMainTopic(topic);
+    }
+
+    @PostMapping("/{id}")
+    ResponseEntity<?> addSubTopic(@PathVariable(value = "id", required = true) Long id, @Valid @RequestBody TopicDto topicDto) {
+        return topicService.addSubTopic(id, topicDto.getSubTitle());
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> removeSubTopic(@PathVariable(value = "id", required = true) Long id, @Valid @RequestBody TopicDto topicDto) {
+        return topicService.removeSubTopic(id, topicDto.getSubTitle());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<?> getTopicById(@PathVariable(value = "id", required = true) Long id) {
+        return topicService.getById(id);
     }
 }
