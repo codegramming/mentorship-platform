05b8f3f5da5c1751f9383934b2a58710e2ddf253
tayfurunal <mtayfurunal@gmail.com>
2020-08-13 17:07:42 +0300

    add annotation to Application class

--- mentorship-backend/src/main/java/com/tayfurunal/mentorship/MentorshipApplication.java
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/MentorshipApplication.java
@@ -2,8 +2,12 @@ package com.tayfurunal.mentorship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.tayfurunal.mentorship.repository.jpa")
+@EnableElasticsearchRepositories("com.tayfurunal.mentorship.repository.elasticsearch")
 public class MentorshipApplication {
 
     public static void main(String[] args) {
