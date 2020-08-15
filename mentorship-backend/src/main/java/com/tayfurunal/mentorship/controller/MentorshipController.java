c4d7e571976ab32a84307c857e4a66fd5ea6cd86
tayfurunal <mtayfurunal@gmail.com>
2020-08-15 16:36:09 +0300

    create mentorship controller

---
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/controller/MentorshipController.java
@@ -0,0 +1,59 @@
+package com.tayfurunal.mentorship.controller;
+
+import com.tayfurunal.mentorship.dto.MentorshipDto;
+import com.tayfurunal.mentorship.payload.ApiError;
+import com.tayfurunal.mentorship.service.MentorshipService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping("/api/mentorships")
+@Api
+public class MentorshipController {
+
+    private MentorshipService mentorshipService;
+
+    public MentorshipController(MentorshipService mentorshipService) {
+        this.mentorshipService = mentorshipService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createMentorship(@Valid @RequestBody MentorshipDto mentorshipDto, Principal principal) {
+        return mentorshipService.createMentorship(mentorshipDto, principal.getName());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> handleValidationException(MethodArgumentNotValidException exception,
+                                                              HttpServletRequest request) {
+        ApiError apiError = new ApiError(false, 400, "Validation Error", request.getServletPath());
+
+        BindingResult result = exception.getBindingResult();
+        Map<String, String> validationErrors = new HashMap<>();
+        for (FieldError fieldError : result.getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        apiError.setValidationErrors(validationErrors);
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+}
