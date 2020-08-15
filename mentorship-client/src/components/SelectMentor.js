c594dde467b234da9e2e4e6b4df2903dbbb37d94
tayfurunal <mtayfurunal@gmail.com>
2020-08-15 16:33:04 +0300

    create select mentor page

---
+++ mentorship-client/src/components/SelectMentor.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Header from './Layout/Header';
+import axios from 'axios';
+
+class SelectMentor extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mainTopic: '',
+      subTopics: '',
+      thoughts: '',
+      username: '',
+      displayName: '',
+      email: '',
+    };
+  }
+
+  getApplication = async (id) => {
+    let application = await axios.get(
+      `http://localhost:8080/api/mentors/${id}?status=ACCEPTED`
+    );
+    this.setState({
+      mainTopic: application.data.mainTopic,
+      subTopics: application.data.subTopics,
+      thoughts: application.data.thoughts,
+      displayName: application.data.user.displayName,
+      username: application.data.user.username,
+      email: application.data.user.email,
+    });
+  };
+
+  startTheProcess = async () => {
+    const { id } = this.props.match.params;
+    const mentorshipDto = {
+      mentorId: id,
+    };
+    await axios
+      .post(`http://localhost:8080/api/mentorships`, mentorshipDto)
+      .then((res) => {
+        const mentorshipId = res.data.id;
+        this.props.history.push(`/mentorshipDetails/${mentorshipId}`);
+      });
+  };
+
+  componentDidMount() {
+    if (!this.props.security.roles[0]?.includes('USER')) {
+      this.props.history.push('/');
+    } else {
+      const { id } = this.props.match.params;
+      this.getApplication(id);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <section
+          class='section section-hero gradient-light--lean-right'
+          style={{ paddingTop: '0px', paddingBottom: '1.4rem' }}
+        >
+          <div class='container'>
+            <div class='row mt-5'>
+              <div class='col-md-8'>
+                <Link to='/adminPanel'>
+                  <small class='text-uppercase text-muted d-inline-block mb-3'>
+                    <i class='fas fa-arrow-left'></i> Back
+                  </small>
+                </Link>
+
+                <h1 class='mb-4'>Start Mentorship</h1>
+
+                <p>If you want to work with this mentor, start the process.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section
+          class='section section-job-description gradient-light--upright'
+          style={{ paddingTop: '2rem' }}
+        >
+          <div class='container'>
+            <div class='row'>
+              <div class='col-md-7 order-md-1'>
+                <h3 class='h2 mb-4'>Who is the Mentor?</h3>
+
+                <p class='mb-5'>
+                  <table class='table'>
+                    <thead class='thead-light'>
+                      <tr>
+                        <th scope='col'>Display Name</th>
+                        <th scope='col'>Username</th>
+                        <th scope='col'>Email</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{this.state.displayName}</td>
+                        <td>{this.state.username}</td>
+                        <td>{this.state.email}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </p>
+
+                <h3 class='h2 mb-4'>Mentor's Skills</h3>
+
+                <ul class='mb-5 list-group'>
+                  <li class='list-group-item active'>{this.state.mainTopic}</li>
+                  <li class='list-group-item'>{this.state.subTopics}</li>
+                </ul>
+
+                <h3 class='h2 mb-4'>Mentor's Thoughts</h3>
+                <p className='mb-4 font-italic' style={{ fontSize: '20px' }}>
+                  {this.state.thoughts}
+                </p>
+
+                <div className='container'>
+                  <div className='row text-center'>
+                    <div className='col'>
+                      <button
+                        href='#0'
+                        style={{ width: '20rem' }}
+                        class='btn btn-success'
+                        onClick={this.startTheProcess}
+                      >
+                        SELECT THE MENTOR
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+SelectMentor.propTypes = {
+  security: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  security: state.security,
+});
+
+export default connect(mapStateToProps, {})(SelectMentor);
