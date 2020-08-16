33c01efabe584f06882ad3131f01c936e8a8b12d
tayfurunal <mtayfurunal@gmail.com>
2020-08-17 01:22:18 +0300

    update mentorship response

--- mentorship-backend/src/main/java/com/tayfurunal/mentorship/payload/MentorshipDetailsResponse.java
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/payload/MentorshipDetailsResponse.java
@@ -1,5 +1,9 @@
 package com.tayfurunal.mentorship.payload;
 
+import com.tayfurunal.mentorship.domain.Phase;
+
+import java.util.List;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -16,10 +20,31 @@ public class MentorshipDetailsResponse {
 
     private String startDate;
 
-    public MentorshipDetailsResponse(String mentorDisplayName, String menteeDisplayName, String status, String startDate) {
+    private Integer numberOfPhases;
+
+    private Boolean hasPhase;
+
+    private String mentorThoughts;
+
+    private String menteeThoughts;
+
+    private Integer currentPhase;
+
+    private List<Phase> phases;
+
+    public MentorshipDetailsResponse(String mentorDisplayName, String menteeDisplayName, String status,
+                                     String startDate, Integer numberOfPhases, Boolean hasPhase,
+                                     String mentorThoughts, String menteeThoughts, Integer currentPhase,
+                                     List<Phase> phases) {
         this.mentorDisplayName = mentorDisplayName;
         this.menteeDisplayName = menteeDisplayName;
         this.status = status;
         this.startDate = startDate;
+        this.numberOfPhases = numberOfPhases;
+        this.hasPhase = hasPhase;
+        this.mentorThoughts = mentorThoughts;
+        this.menteeThoughts = menteeThoughts;
+        this.currentPhase = currentPhase;
+        this.phases = phases;
     }
 }
