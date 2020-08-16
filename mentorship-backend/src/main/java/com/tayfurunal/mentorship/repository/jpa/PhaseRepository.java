8088e5c3f0d630176d642848cd94aaa1f996e392
tayfurunal <mtayfurunal@gmail.com>
2020-08-17 01:17:46 +0300

    create phase repository

---
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/repository/jpa/PhaseRepository.java
@@ -0,0 +1,8 @@
+package com.tayfurunal.mentorship.repository.jpa;
+
+import com.tayfurunal.mentorship.domain.Phase;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhaseRepository extends JpaRepository<Phase, Long> {
+}
