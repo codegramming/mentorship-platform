3912c5d0cbe59389cbaf15ad62218e23ea775598
tayfurunal <mtayfurunal@gmail.com>
2020-08-20 23:09:48 +0300

    add findall queries to mentorship repository

--- mentorship-backend/src/main/java/com/tayfurunal/mentorship/repository/jpa/MentorshipRepository.java
+++ mentorship-backend/src/main/java/com/tayfurunal/mentorship/repository/jpa/MentorshipRepository.java
@@ -1,12 +1,20 @@
 package com.tayfurunal.mentorship.repository.jpa;
 
+import com.tayfurunal.mentorship.domain.Mentee;
+import com.tayfurunal.mentorship.domain.Mentor;
 import com.tayfurunal.mentorship.domain.Mentorship;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MentorshipRepository extends JpaRepository<Mentorship, Long> {
 
     Mentorship getById(Long id);
+
+    List<Mentorship> findAllByMentor(Mentor mentor);
+
+    List<Mentorship> findAllByMentee(Mentee mentee);
 }
